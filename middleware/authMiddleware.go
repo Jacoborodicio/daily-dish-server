0714193503549ff933be3a5a8ecdb5b349779493
Jacobo Rodicio González <jacobo.rodicio@it-manufactory.com>
2023-07-02 15:36:49 +0200

    refactor: change unauthorized status

--- middleware/authMiddleware.go
+++ middleware/authMiddleware.go
@@ -12,13 +12,13 @@ func Authenticate() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		clientToken := c.Request.Header.Get("token")
 		if clientToken == "" {
-			c.JSON(http.StatusInternalServerError, gin.H{"error": fmt.Sprintf("No Authorization Token provided")})
+			c.JSON(http.StatusUnauthorized, gin.H{"error": fmt.Sprintf("No Authorization Token provided")})
 			c.Abort()
 			return
 		}
 		claims, err := helpers.ValidateToken(clientToken)
 		if err != "" {
-			c.JSON(http.StatusInternalServerError, gin.H{"error": err})
+			c.JSON(http.StatusUnauthorized, gin.H{"error": err})
 			c.Abort()
 			return
 		}
