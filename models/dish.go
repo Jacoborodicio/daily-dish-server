a654ff5a124ba6c8b34e3cde4a9ce8bf31c4e0af
Jacobo Rodicio González <jacobo.rodicio@it-manufactory.com>
2023-12-26 12:46:01 +0100

    feat: extend dishes to allow public/private dishes

--- models/dish.go
+++ models/dish.go
@@ -1,17 +1,20 @@
 package models
+
 import (
-  "go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
 type Dish struct {
-	Name            *string            `json:"name"`
-	Fat             *float64           `json:"fat"`
-	Ingredients     *[]primitive.ObjectID            `bson:"ingredients"`
-	Recipe          *string            `json:"recipe"`
-	Calories        *float64           `json:"calories"`
-	PreparationTime *string            `json:"preparationTime"`
-  Tags            *[]Tag             `json:tags` 
-  Categories        *[]primitive.ObjectID            `bson:"categories"`
-  // Image           *string            `json:"image"`
-  // Steps           *string            `json:"steps"`
+	Name            *string               `json:"name"`
+	Fat             *float64              `json:"fat"`
+	Ingredients     *[]primitive.ObjectID `bson:"ingredients"`
+	Recipe          *string               `json:"recipe"`
+	Calories        *float64              `json:"calories"`
+	PreparationTime *string               `json:"preparationTime"`
+	Tags            *[]Tag                `json:"tags"`
+	Categories      *[]primitive.ObjectID `bson:"categories"`
+	Public          *bool                 `json:"public"`
+	UserID          *string               `json:"userid"`
+	// Image           *string            `json:"image"`
+	// Steps           *string            `json:"steps"`
 }
--- routes/dishesRouter.go
+++ routes/dishesRouter.go
@@ -12,9 +12,9 @@ import (
 	"github.com/go-playground/validator/v10"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
-  "go.mongodb.org/mongo-driver/mongo/options"
 	"go.mongodb.org/mongo-driver/mongo"
-  "strconv"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"strconv"
 )
 
 var validate = validator.New()
@@ -23,6 +23,7 @@ var ingredientsColelction *mongo.Collection = database.OpenCollection(database.C
 
 func AddDish(c *gin.Context) {
 	var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+	defer cancel()
 	var dish models.Dish
 	if err := c.BindJSON(&dish); err != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
@@ -42,22 +43,30 @@ func AddDish(c *gin.Context) {
 		fmt.Println(insertErr)
 		return
 	}
-	defer cancel()
 	c.JSON(http.StatusOK, result)
 }
 
 func GetDishes(c *gin.Context) {
 	var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
-  page, _ := strconv.ParseInt(c.DefaultQuery("page", "1"), 10, 64)
-  limit, _ := strconv.ParseInt(c.DefaultQuery("limit", "0"), 10, 64)
-  offset := (page - 1) * limit
-  
-  options := options.Find()
-  options.SetLimit(limit)
-  options.SetSkip(offset)
-  
+	defer cancel()
+	userId := c.Query("userid")
+	page, _ := strconv.ParseInt(c.DefaultQuery("page", "1"), 10, 64)
+	limit, _ := strconv.ParseInt(c.DefaultQuery("limit", "0"), 10, 64)
+	offset := (page - 1) * limit
+
+	options := options.Find()
+	options.SetLimit(limit)
+	options.SetSkip(offset)
+
 	var dishes []bson.M
-  cursor, err := dishCollection.Find(ctx, bson.M{}, options)
+	var filter bson.M
+	if userId != "" {
+		filter = bson.M{"userid": userId}
+	} else {
+		filter = bson.M{"public": true}
+		// filter = bson.M{}
+	}
+	cursor, err := dishCollection.Find(ctx, filter, options)
 
 	if err != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
@@ -70,49 +79,54 @@ func GetDishes(c *gin.Context) {
 		return
 	}
 	defer cancel()
-	fmt.Println(dishes)
-	c.JSON(http.StatusOK, dishes)
+	if len(dishes) == 0 {
+		c.JSON(http.StatusOK, []bson.M{})
+	} else {
+		c.JSON(http.StatusOK, dishes)
+	}
 }
+
 func GetDishById(c *gin.Context) {
 	dishID := c.Params.ByName("id")
 	docId, _ := primitive.ObjectIDFromHex(dishID)
 	var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+	defer cancel()
 	var dish []bson.M
 
-  pipeline := []bson.M{
-   bson.M{"$lookup": bson.M{
-   "from": "ingredients",
-   "localField": "ingredients",
-   "foreignField": "_id",
-   "as": "ingredients",
-   }},
-   bson.M{"$lookup": bson.M{
-   "from": "categories",
-   "localField": "categories",
-   "foreignField": "_id",
-   "as": "categories",
-   }},
-   bson.M{"$match": bson.M{"_id": docId}},
-   }
-   cursor, err := dishCollection.Aggregate(ctx, pipeline)
-   if err != nil {
-   c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
-   return
-   }
-   if err = cursor.All(ctx, &dish); err != nil {
-   c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
-   fmt.Println(err)
-   return
-   }
-  aggregatedDish := dish[0];
-   defer cancel()
-  c.JSON(http.StatusOK, aggregatedDish)
+	pipeline := []bson.M{
+		bson.M{"$lookup": bson.M{
+			"from":         "ingredients",
+			"localField":   "ingredients",
+			"foreignField": "_id",
+			"as":           "ingredients",
+		}},
+		bson.M{"$lookup": bson.M{
+			"from":         "categories",
+			"localField":   "categories",
+			"foreignField": "_id",
+			"as":           "categories",
+		}},
+		bson.M{"$match": bson.M{"_id": docId}},
+	}
+	cursor, err := dishCollection.Aggregate(ctx, pipeline)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	if err = cursor.All(ctx, &dish); err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		fmt.Println(err)
+		return
+	}
+	aggregatedDish := dish[0]
+	c.JSON(http.StatusOK, aggregatedDish)
 }
 
 func UpdateDish(c *gin.Context) {
 	dishId := c.Params.ByName("id")
 	docId, _ := primitive.ObjectIDFromHex(dishId)
 	ctx, cancel := context.WithTimeout(context.Background(), 100*time.Second)
+	defer cancel()
 	var dish, updatedDish models.Dish
 	if err := c.BindJSON(&dish); err != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
@@ -151,13 +165,20 @@ func UpdateDish(c *gin.Context) {
 	if dish.PreparationTime != nil {
 		updatedDish.PreparationTime = dish.PreparationTime
 	}
-  if dish.Tags != nil {
-    updatedDish.Tags = dish.Tags
-  }
+	if dish.Tags != nil {
+		updatedDish.Tags = dish.Tags
+	}
 
-  if dish.Categories != nil {
-    updatedDish.Categories = dish.Categories
-  }
+	if dish.Public != nil {
+		updatedDish.Public = dish.Public
+	}
+	if dish.UserID != nil {
+		updatedDish.UserID = dish.UserID
+	}
+
+	if dish.Categories != nil {
+		updatedDish.Categories = dish.Categories
+	}
 
 	result, err := dishCollection.ReplaceOne(
 		ctx,
@@ -169,8 +190,10 @@ func UpdateDish(c *gin.Context) {
 			"recipe":          updatedDish.Recipe,
 			"calories":        updatedDish.Calories,
 			"preparationTime": updatedDish.PreparationTime,
-      "categories":      updatedDish.Categories,
-      "tags":            updatedDish.Tags,
+			"categories":      updatedDish.Categories,
+			"tags":            updatedDish.Tags,
+			"public":          updatedDish.Public,
+			"userid":          updatedDish.UserID,
 		},
 	)
 	if err != nil {
@@ -178,7 +201,6 @@ func UpdateDish(c *gin.Context) {
 		fmt.Println(err)
 		return
 	}
-	defer cancel()
 	c.JSON(http.StatusOK, result.ModifiedCount)
 }
 
@@ -186,6 +208,7 @@ func DeleteDish(c *gin.Context) {
 	dishId := c.Params.ByName("id")
 	docId, _ := primitive.ObjectIDFromHex(dishId)
 	var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+	defer cancel()
 	result, err := dishCollection.DeleteOne(ctx, bson.M{"_id": docId})
 
 	if err != nil {
@@ -193,7 +216,5 @@ func DeleteDish(c *gin.Context) {
 		fmt.Println(err)
 		return
 	}
-
-	defer cancel()
 	c.JSON(http.StatusOK, result.DeletedCount)
 }
